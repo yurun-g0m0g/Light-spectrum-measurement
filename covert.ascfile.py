4a301563b04032937ee2dfe3b67185770ac40094
yurun_g0m0g <yukiyanagi9685@gmail.com>
2024-04-10 13:19:51 +0900

    テスト

---
+++ covert.ascfile.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import os
+
+# 変換したいファイルがあるフォルダのパス
+folder_path = '/Users/sidareyanagi542/Desktop/授業資料/4年/0405'
+
+# 変換されたファイル数をカウントする変数
+converted_files_count = 0
+
+# 指定されたフォルダ内の全ての.ascファイルを探索
+for filename in os.listdir(folder_path):
+    if filename.endswith(".asc"):
+        # ファイルパスの作成
+        file_path = os.path.join(folder_path, filename)
+        
+        # .ascファイルを読み込み
+        data = pd.read_csv(file_path, delimiter='\t') # ここで、データの区切り文字を指定します。タブが標準ですが、必要に応じて変更してください。
+        
+        # Excelファイル名の作成（.ascを.xlsxに変更）
+        excel_filename = filename.replace('.asc', '.xlsx')
+        excel_path = os.path.join(folder_path, excel_filename)
+        
+        # Excelファイルとして保存
+        data.to_excel(excel_path, index=False)
+
+        # 変換されたファイル数をインクリメント
+        converted_files_count += 1
+
+# 変換されたファイル数の表示
+print(f"全ての.ascファイルが.xlsxに変換されました。変換されたファイル数: {converted_files_count}")
---
+++ rearrangement_elsxfile.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import os
+
+# 変換したいファイルがあるフォルダのパス
+folder_path = '/Users/sidareyanagi542/Desktop/授業資料/4年/0405'
+
+# xlsxファイルの数をカウントする変数
+xlsx_file_count = 0
+
+# 指定されたフォルダ内の全ての.xlsxファイルを探索
+for filename in os.listdir(folder_path):
+    if filename.endswith(".xlsx"):
+        # xlsxファイルのカウントを増やす
+        xlsx_file_count += 1
+
+        # Excelファイルのフルパス
+        file_path = os.path.join(folder_path, filename)
+
+        # Excelファイルを読み込む際に、engineを明示的に指定する
+        df = pd.read_excel(file_path, engine='openpyxl')
+        
+        # A列のデータをスペースで区切って新しい列に分割
+        split_columns = df.iloc[:, 0].str.split(' ', expand=True)
+        
+        # 分割されたデータを元のデータフレームに結合
+        for i, column in enumerate(split_columns.columns):
+            df[f'New_Column{i+1}'] = split_columns[column]
+        
+        # 変更を同じファイルに保存
+        df.to_excel(file_path, index=False, engine='openpyxl')
+
+# 処理が終わったら、xlsxファイルの総数を出力
+print(f"処理された.xlsxファイルの総数: {xlsx_file_count}")
